4da0839db0e1fecdc45067076fa0980c46944c83
Karen Roh <karenroh@hotmail.com>
2021-04-26 15:48:52 -0400

    routes static files to backend

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+//routes our static files aka our frontend code to our backend
+//if we didn't do this, none of our code within the public folder would've worked
+//meaning the css file, the js files
+app.use(express.static("./public"));
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
