f63a0b99e9436f94ff83dc8907e7cbcf6a8e200d
Karen Roh <karenroh@hotmail.com>
2021-04-26 18:00:17 -0400

    GET /notes returns the notes.html file and GET * returns the index.html file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -6,4 +7,13 @@ const app = express();
 //if we didn't do this, none of our code within the public folder would've worked
 //meaning the css file, the js files
 app.use(express.static("./public"));
+
+app.get("/notes", (req, res) => {
+  res.sendFile(`${__dirname}/public/notes.html`);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
